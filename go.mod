9a689263ebad0fe1dc5b9b4e16f3e1c1ed505275
Raksit Nongbua <tan.raksit@gmail.com>
2023-11-18 20:32:32 +0700

    feat: add pdf service

---
+++ go.mod
@@ -0,0 +1,36 @@
+module pdf-service
+
+go 1.21.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.11.3
+	github.com/unidoc/unipdf/v3 v3.52.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/i18n v0.0.0-20150820051429-8b358169da46 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/unidoc/pkcs7 v0.2.0 // indirect
+	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
+	github.com/unidoc/unichart v0.1.0 // indirect
+	github.com/unidoc/unitype v0.2.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/image v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/labstack/echo/v4"
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/creator"
+	"github.com/unidoc/unipdf/v3/model"
+	"github.com/joho/godotenv"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func uploadFileHandler(c echo.Context) error {
+	_, err := c.MultipartForm()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+	}
+
+	file, _, err := c.Request().FormFile("file")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+	}
+	defer file.Close()
+
+	tempFile, err := os.CreateTemp("./uploads", "temp.pdf")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ErrorResponse{Error: err.Error()})
+	}
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ErrorResponse{Error: err.Error()})
+	}
+
+	// TODO: the password must be encrypted and add the logic decrypt here.
+	password := c.Request().FormValue("password")
+	fmt.Println("password: ", password)
+
+	err = decryptPDF(tempFile.Name(), password)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
+	}
+	return c.File(tempFile.Name())
+}
+
+func decryptPDF(pathFile, password string) error {
+	file, err := os.Open(pathFile)
+	if err != nil {
+		return fmt.Errorf("error opening encrypted PDF file: %v", err)
+	}
+	defer file.Close()
+
+	pdfReader, err := model.NewPdfReader(file)
+	if err != nil {
+		return fmt.Errorf("error creating PDF reader: %v", err)
+	}
+
+	authenticated, err := pdfReader.Decrypt([]byte(password))
+	if err != nil {
+		return fmt.Errorf("error decrypting PDF: %v", err)
+	}
+	if !authenticated {
+		return fmt.Errorf("authentication failed: incorrect password")
+	}
+
+	pdfWriter := creator.New()
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return fmt.Errorf("error getting number of pages: %v", err)
+	}
+	for pageNum := 1; pageNum <= numPages; pageNum++ {
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return fmt.Errorf("error getting PDF page: %v", err)
+		}
+		pdfWriter.AddPage(page)
+	}
+
+	err = pdfWriter.WriteToFile(pathFile)
+	if err != nil {
+		return fmt.Errorf("error writing decrypted PDF to file: %v", err)
+	}
+
+	return nil
+}
+
+// TODO: split middleware and pdf unlock service.
+func main() {
+	err := os.MkdirAll("./uploads", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating uploads directory:", err)
+		return
+	}
+	godotenv.Load(".env")
+	metredKey := os.Getenv("UNIDOC_METERED_LICENSE_KEY")
+	fmt.Println("ENV:" , metredKey)
+	license.SetMeteredKey(metredKey)
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+
+	e.GET("/api", func(c echo.Context) error {
+		return c.String(http.StatusOK, "api is ready!")
+	})
+
+	e.POST("/api/unlock-pdf", uploadFileHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
